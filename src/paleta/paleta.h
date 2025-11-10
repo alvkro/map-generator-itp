a43250109cb2503728737ce677decd73b20fb40a
Álvaro Barbosa Filho <alvaro.filho.111@ufrn.edu.br>
2025-11-10 13:54:15 -0300

    paleta.h: comecie a definir os tipos/classes

---
+++ src/paleta/paleta.h
@@ -0,0 +1,23 @@
+using namespace std;
+
+struct Cores {
+    int r, g, b;
+};
+
+class Paleta {
+    private:
+    Cores* ConjuntoDeCores;
+    int tamanho, capacidade;
+    
+    public:
+
+    Paleta() {
+        capacidade = 255;
+        tamanho = 0;
+        ConjuntoDeCores = new Cores[capacidade];
+    }
+
+    ~Paleta() {
+        delete[] ConjuntoDeCores;
+    }
+};
