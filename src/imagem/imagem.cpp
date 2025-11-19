d84514cfa5fc120dfbe32c62556477bb341fa754
Álvaro Barbosa Filho <alvaro.filho.111@ufrn.edu.br>
2025-11-19 10:18:27 -0300

    feat: Refatorei a função lerPPM, mas ainda falta desenvolver a função criarImagem

--- src/imagem/imagem.cpp
+++ src/imagem/imagem.cpp
@@ -7,32 +7,42 @@ int Imagem::obterAltura() {return altura;}
 
 int Imagem::obterLargura() {return largura;}
 
+void Imagem::criarImagem(int w, int h) { // ???
+    largura = w;
+    altura = h;
+}
+
 bool Imagem::lerPPM(string arquivo) {
-        ifstream arquivo;
-        if (!arquivo.is_open())
-            return false;
-
-        string tipo;
-        arquivo >> tipo;
-
-        if (tipo != "P3")
-            return false;
-
-        int w, h, maxVal;
-        arquivo >> w >> h >> maxVal;
-
-        criarImagem(w, h);
-
-        for (int y = 0; y < h; y++) {
-            for (int x = 0; x < w; x++) {
-                arq >> dadosPixel[y][x].r
-                    >> dadosPixel[y][x].g
-                    >> dadosPixel[y][x].b;
-            }
-        }
-
-        return true;
+    ifstream arq(arquivo);
+    if (!arq.is_open()) {
+        return false;
     }
+
+    string tipo;
+    arq >> tipo;
+
+    if (tipo != "P3") { // apenas arquivos P3 simples
+        return false;
+    }
+
+    int w, h, maxVal;
+
+    arq >> w >> h >> maxVal;
+
+    criarImagem(w, h); // cria a matriz
+
+    // Leitura simples dos pixels
+    for (int y = 0; y < h; y++) {
+        for (int x = 0; x < w; x++) {
+            arq >> dadosPixel[y][x].r;
+            arq >> dadosPixel[y][x].g;
+            arq >> dadosPixel[y][x].b;
+        }
+    }
+
+    return true;
+}
+
 
 Pixel& Imagem::operator()(int x, int y) {
     return dadosPixel[y][x];
