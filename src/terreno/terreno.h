8dfade40923196a7a61e2c2476418e0cd0b47a04
Álvaro Barbosa Filho <alvaro.filho.111@ufrn.edu.br>
2025-11-28 13:22:29 -0300

    fix: consertei um erro no destrutor que não deletava a array toda e sim apenas uma parte dela + algumas correções mínimas

--- src/terreno/terreno.h
+++ src/terreno/terreno.h
@@ -3,33 +3,31 @@
 
 class Terreno {
 private:
-int tamanho, w, h;
+int tamanho;
 int** MedidasTerreno; // dá pra fazer diferente? (NAO)
 
 public:
 Terreno(int area) {
     tamanho = pow(2, area) + 1;
-    w = tamanho;
-    h = tamanho;
     gerarTerreno();
 }
 
 ~Terreno() {
-    for (int i = 0; i < w; i++) {
+    for (int i = 0; i < tamanho; i++) {
         delete[] MedidasTerreno[i];
     }
-    delete[] MedidasTerreno[h];
+    delete[] MedidasTerreno;
 }
 
-Terreno operator()(int x, int y);
+int& operator()(int x, int y);
 void gerarTerreno();
 int obterLargura();
 int obterProfundidade();
-double randomizador(int antiDeslocamento, int deslocamento);
+int randomizador(int antiDeslocamento, int deslocamento);
 void diamondSquare(int size, int ruido);
 int quantidadeLinhas(int area);
 int quantidadeColunas(int area);
-double square_step(int size, int ruido);
-int returnAltitude(int** terreno, int x, int y);
-double diamond_step(int size, int ruido);
+int square_step(int step, int ruido);
+int returnAltitude(int x, int y);
+int diamond_step(int size, int ruido);
 };
