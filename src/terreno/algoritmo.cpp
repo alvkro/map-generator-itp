6b0c384551f2e5b366fed2bbe1d4b40ebfa0b92c
Álvaro Barbosa Filho <alvaro.filho.111@ufrn.edu.br>
2025-11-28 13:24:18 -0300

    fix: alterei o type das etapas da diamond-square (double -> int)

--- src/terreno/algoritmo.cpp
+++ src/terreno/algoritmo.cpp
@@ -5,43 +5,44 @@ using namespace std;
 
 // Deslocamento do Ponto Médio:
 
-double Terreno::randomizador(int antiDeslocamento, int deslocamento) {
+int Terreno::randomizador(int antiDeslocamento, int deslocamento) {
     random_device rd;
     mt19937 gen(rd());
     uniform_int_distribution<> dist(antiDeslocamento, deslocamento);
     return dist(gen);
 }
 
-double Terreno::square_step(int size, int ruido) {
-    // POR ENQUANTO ESTÁ SEM O RUÍDO, PARA FINS DE TESTE!!!!
-    int step = tamanho / 2;
-    for (int x = 0; x < size - 1; x += step) {  
-            for (int y = 0; y < size - 1; y += step) {
-                int avg = (MedidasTerreno[x][y] + MedidasTerreno[x][y + step] +  
-                    MedidasTerreno[x + step][y] + MedidasTerreno[x + step][y + step])/4;
+int Terreno::square_step(int step, int ruido) { // size vai receber tamanho...
+    for (int x = 0; x < tamanho - 1; x += step) {  
+            for (int y = 0; y < tamanho - 1; y += step) {
+                int avg = (MedidasTerreno[x][y] + 
+                    MedidasTerreno[x][y + step] +  
+                    MedidasTerreno[x + step][y] + 
+                    MedidasTerreno[x + step][y + step]) / 4.0;
+
+                    avg += randomizador(-ruido, ruido);
                 
                     int midX = x + step/2;
                     int midY = y + step/2;
 
                     MedidasTerreno[midX][midY] = avg;
-                    return MedidasTerreno[midX][midY];
             }
         }
+    return MedidasTerreno[tamanho / 2][tamanho / 2];
 }
 
-double Terreno::diamond_step(int size, int ruido) {
-
-    int step = tamanho / 2;
+int Terreno::diamond_step(int size, int ruido) {
+    int step = size / 2;
     for (int x = step / 2; x < size; x += step) {
         for (int y = step / 2; y < size; y += step) {
-            double corner1 = MedidasTerreno[x - (step/2)][y - (step/2)];
-            double corner2 = MedidasTerreno[x - (step/2)][y + (step/2)];
-            double corner3 = MedidasTerreno[x + (step/2)][y - (step/2)];
-            double corner4 = MedidasTerreno[x + (step/2)][y + (step/2)];
+            int corner1 = MedidasTerreno[x - (step/2)][y - (step/2)];
+            int corner2 = MedidasTerreno[x - (step/2)][y + (step/2)];
+            int corner3 = MedidasTerreno[x + (step/2)][y - (step/2)];
+            int corner4 = MedidasTerreno[x + (step/2)][y + (step/2)];
 
-            double mid = (corner1 + corner2 + corner3 + corner4)/4;
-            double randomvalue = randomizador(-ruido, ruido);
-            MedidasTerreno[y][x] = mid + randomvalue;    
+            int mid = (corner1 + corner2 + corner3 + corner4)/4;
+            int randomvalue = randomizador(-ruido, ruido);
+            MedidasTerreno[x][y] = mid + randomvalue;  
         }
     }
 }
@@ -59,6 +60,6 @@ void Terreno::diamondSquare(int size, int ruido) {
     while (step > 1) {
     square_step(size, ruido);
     diamond_step(size, ruido);
-    }
     step /= 2;
+    }
 }
